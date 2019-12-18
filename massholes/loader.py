cfdd54efc964a8f7b6494ad2fa8a83a487cbc7fc
Kevin Freeman <Kevin.Freeman@dsm.com>
2019-12-18 12:12:52 -0500

    remove series, use assign

--- massholes/loader.py
+++ massholes/loader.py
@@ -6,10 +6,13 @@ def loader(path):
     Load the file associated with the rats and onions experiment.
     """
     df = pd.read_excel(path, index_col=0)
-    treatment = pd.read_excel(path, sheet_name='y')
-    treatment = pd.Series(treatment['y'])
+    treatment = pd.read_excel(path, sheet_name='y', usecols=range(10, 15))
 
-    new_df = df.transpose().reset_index()
-    new_df['treatment'] = treatment
+    new_df = (
+        df
+        .transpose()
+        .reset_index()
+        .assign(treatment=treatment['y'])
+    )
 
     return new_df
