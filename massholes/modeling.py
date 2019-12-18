5965ab15c163af2803ba1e91a613e73c14d7f208
Kevin Freeman <Kevin.Freeman@dsm.com>
2019-12-18 16:13:59 -0500

    add BandSelector estimator

--- massholes/modeling.py
+++ massholes/modeling.py
@@ -11,3 +11,21 @@ class ParetoScaler(BaseEstimator, TransformerMixin):
         pareto = np.sqrt(np.std(X, axis=1))
         X_trans = np.divide(X, pareto.to_frame()) 
         return X_trans
+
+
+class BandSelector(BaseEstimator, TransformerMixin):
+    """
+    Band selector selects a spectra between start and stop
+    Example:
+    selector = BandSelector(start = 1, stop = 3)
+    selector.fit_transform(spectra_df)
+    """
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        return X.loc[:, self.start:self.stop]
