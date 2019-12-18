bcda8bd8a8feef6229dc507e45790da9fb900241
Claypool <Joshua.Claypool@dsm.com>
2019-12-18 11:53:23 -0500

    linted the heck out the loader and added setup

--- massholes/loader.py
+++ massholes/loader.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
+
 def loader(path):
-    df = pd.read_excel(path,index_col=0)
-    treatment=pd.read_excel(path,sheet_name='y')
-    treatment=pd.Series(treatment['y'])
+    """
+    Load the file associated with the rats and onions experiment.
+    """
+    df = pd.read_excel(path, index_col=0)
+    treatment = pd.read_excel(path, sheet_name='y')
+    treatment = pd.Series(treatment['y'])
 
-    new_df=df.transpose().reset_index()
-    new_df['treatment']=treatment
-
+    new_df = df.transpose().reset_index()
+    new_df['treatment'] = treatment
 
     return new_df
