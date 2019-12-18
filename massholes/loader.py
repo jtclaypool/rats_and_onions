d191b9f455118749a5f85e50accd3b3e9f1b1fe6
Cor Zuurmond <jczuurmond@protonmail.com>
2019-12-18 13:22:40 -0500

    Load excel file once instead of twice

--- massholes/loader.py
+++ massholes/loader.py
@@ -5,8 +5,9 @@ def loader(path):
     """
     Load the file associated with the rats and onions experiment.
     """
-    df = pd.read_excel(path, index_col=0)
-    treatment = pd.read_excel(path, sheet_name='y', usecols=range(10, 15))
+    xl = pd.ExcelFille(path)
+    df = pd.read_excel(xl, index_col=0)
+    treatment = pd.read_excel(xl, sheet_name='y', usecols=range(10, 15))
 
     new_df = (
         df
